655369813e472b056c46b1a32135f3548692cab4
Hugh Bellamy <hughbellars@gmail.com>
2020-10-31 11:11:59 +0000

    Cleanup tests and code

--- Sources/MsgReader/EmbeddedMessage.swift
+++ Sources/MsgReader/EmbeddedMessage.swift
@@ -12,34 +12,38 @@ public struct EmbeddedMessage: MessageStorageInternal, CustomStringConvertible {
     internal var properties: PropertyStream<EmbeddedMessagePropertiesHeader>
     internal var namedProperties: NamedPropertyMapping?
     
-    public private(set) var recipients = [Recipient]()
-    public private(set) var attachments = [Attachment]()
+    public let recipients: [Recipient]
+    public let attachments: [Attachment]
 
     internal init(storage: inout CompoundFileStorage, namedProperties: NamedPropertyMapping?) throws {
         self.properties = try PropertyStream(parent: storage)
         self.namedProperties = namedProperties
         
-        for child in storage.children {
-            if child.key.hasPrefix("__recip_version1.0_") {
-                let recipient = try Recipient(storage: child.value, namedProperties: namedProperties)
+        var recipients: [Recipient] = []
+        var attachments: [Attachment] = []
+        for (name, storage) in storage.children {
+            if name.hasPrefix("__recip_version1.0_") {
+                let recipient = try Recipient(storage: storage, namedProperties: namedProperties)
                 recipients.append(recipient)
-            } else if child.key.hasPrefix("__attach_version1.0_") {
-                let attachment = try Attachment(storage: child.value, namedProperties: namedProperties)
+            } else if name.hasPrefix("__attach_version1.0_") {
+                let attachment = try Attachment(storage: storage, namedProperties: namedProperties)
                 attachments.append(attachment)
             }
         }
         
         recipients.sort { ($0.rowid ?? 0) < ($1.rowid ?? 0) }
         attachments.sort { ($0.attachNumber ?? 0) < ($1.attachNumber ?? 0) }
+        
+        self.recipients = recipients
+        self.attachments = attachments
     }
 
     /// [MS-OXMSG] 2.1.1 Properties of a .msg File
     /// [MS-OXMSG] 2.1.1.1 PidTagStoreSupportMask
     /// Type: PtypInteger32 ([MS-OXCDATA] section 2.11.1)
-    /// The PidTagStoreSupportMask property ([MS-OXPROPS] section 2.1024) indicates whether string
-    /// properties within the .msg file are Unicode-encoded or not. This property defines multiple flags, but
-    /// only the STORE_UNICODE_OK flag is valid. All other bits MUST be ignored. The settings for this
-    /// property are summarized in the following table.
+    /// The PidTagStoreSupportMask property ([MS-OXPROPS] section 2.1024) indicates whether string properties within the .msg file
+    /// are Unicode-encoded or not. This property defines multiple flags, but only the STORE_UNICODE_OK flag is valid. All other bits
+    /// MUST be ignored. The settings for this property are summarized in the following table. 
     var storeSupportMask: StoreSupportMask? {
         guard let rawValue: UInt32 = getProperty(id: .tagStoreSupportMask) else {
             return nil
